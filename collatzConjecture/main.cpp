82ab01d6b77025999932dae48cf50d4094152d9b
gabiavila4 <123492198+gabiavila4@users.noreply.github.com>
2025-09-10 10:37:06 -0500

    Initial Commit

---
+++ collatzConjecture/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  collatzConjecture
+//
+//  Created by Gabriela Avila on 9/10/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
