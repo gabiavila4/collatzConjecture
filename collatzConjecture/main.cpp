f10b309fe85047c7a4502dfca96b87118b4dad35
gabiavila4 <123492198+gabiavila4@users.noreply.github.com>
2025-09-14 21:03:57 -0500

    9/14

--- collatzConjecture/main.cpp
+++ collatzConjecture/main.cpp
@@ -6,9 +6,53 @@
 //
 
 #include <iostream>
+#include <vector>
 
-int main(int argc, const char * argv[]) {
+using namespace std;
+
+void collatz(int n)
+{
+   
+    
+    if (n == 1)
+    {
+        cout << n ;
+        return;
+    }else{
+        cout << n << "-> " ;
+    }
+   if(n % 2 == 0)
+   {
+       collatz(n/2);
+   }else{
+       collatz((3 * n) + 1);
+   }
+    
+    //return myVector;
+}
+
+int main() {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    int num;
+    bool go = true;
+    //bool goo = true;
+    char yorn;
+    cout << "What number do you want to try first ? \n";
+    cin >> num;
+    collatz(num);
+    while (go)
+    {
+        cout << "\nDo you want to try again (y/n)? ";
+        cin >> yorn;
+        if (yorn == 'y' || yorn == 'Y')
+        {
+            cout << "What number do you want to try next ? \n";
+            cin >> num;
+            collatz(num);
+        }else {
+            go = false;
+        }
+    }
+    
     return 0;
 }
